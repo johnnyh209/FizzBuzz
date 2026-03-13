fda9fbb0e5fee2d5ff191e6b4eb301b06737ee66
johnnyh209 <33064730+johnnyh209@users.noreply.github.com>
2026-03-13 19:31:13 -0400

    Upload project files

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("FizzBuzz!");
+
+for (int i = 0; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+        Console.WriteLine("fizzbuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine("fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine("buzz");
+    else
+        Console.WriteLine(i);
+}
